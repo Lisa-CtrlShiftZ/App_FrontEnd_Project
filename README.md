88bcd1276a9765a070810642404c36ef740bc8aa
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2025-01-08 10:39:55 +0100

    New base files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FrontEndFinalProject
+# FrontendFinalproject
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'FrontEndFinalProject' title`, () => {
+  it(`should have the 'frontend_finalproject' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FrontEndFinalProject');
+    expect(app.title).toEqual('frontend_finalproject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FrontEndFinalProject');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend_finalproject');
   });
 });
