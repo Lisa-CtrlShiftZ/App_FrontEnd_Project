d02ec1cc7f19ebae3a77c8a6b669a613ba90738e
BtheBelgian <147116285+BtheBelgian@users.noreply.github.com>
2025-01-13 13:43:57 +0100

    Create routes for all onboarding pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,49 @@
 import { Routes } from '@angular/router';
+import {OnboardingComponent} from './onboarding/gegevens/onboarding.component';
+import { ToevoegenComponent } from './onboarding/toevoegen/toevoegen.component';
+import { PersonenComponent } from './onboarding/personen/personen.component';
+import { DuurComponent } from './onboarding/duur/duur.component';
+import { DieetComponent } from './onboarding/dieet/dieet.component';
+import { BedanktComponent } from './onboarding/bedankt/bedankt.component';
 
 export const routes: Routes = [
     {
         path: 'onboarding',
+        component: OnboardingComponent,
+    },
+    {
+        path: 'toevoegen',
+        component: ToevoegenComponent,
+        title: 'toevoegen',
+    },
+    {
+        path: 'personen',
+        component: PersonenComponent,
+        title: 'personen',
+    },
+    {
+        path: 'duur',
+        component: DuurComponent,
+        title: 'duur',
+    },
+    {
+        path: 'dieet',
+        component: DieetComponent,
+        title: 'dieet',
+    },
+    {
+        path: 'bedankt',
+        component: BedanktComponent,
+        title: 'bedankt',
+    },
+];
+
+
+        /* path: 'gegevens',
         loadComponent: () => {
             return import('./onboarding/gegevens/onboarding.component').then((m) => m.OnboardingComponent)
         },
-    }
-];
+        path: 'toevoegen',
+        loadComponent: () => {
+            return import('./onboarding/toevoegen/toevoegen.component').then((m) => m.ToevoegenComponent)
+        } */
