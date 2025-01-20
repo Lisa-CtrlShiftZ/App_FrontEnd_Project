3074429c2cbb85d6d1fc0bd9bd8cb565b3ab99ad
BtheBelgian <147116285+BtheBelgian@users.noreply.github.com>
2025-01-20 14:47:42 +0100

    Add addPerson function to personen

--- src/app/onboarding/personen/personen.component.ts
+++ src/app/onboarding/personen/personen.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { OnboardingService } from '../../onboarding.service';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-personen',
@@ -12,21 +19,62 @@ import { RouterModule } from '@angular/router';
 export class PersonenComponent {
 
   formData = {
-    voornaam: '',
-    familienaam: '',
-    geboortedatum: '',
-    gewicht: '',
+    name: '',
+    last_name: '',
+    gender: '',
+    height: 0,
+    weight: 0,
+    diet: '',
+    date_of_birth: '' ,
   }
-  constructor(private router: Router) {
+
+  constructor(private router: Router, private onboardingService: OnboardingService) {
         //Load saved data from sessionStorage if there is any.
         const savedData = sessionStorage.getItem('formData');
         if (savedData) {
           this.formData = JSON.parse(savedData);
         }
   }
-  
+
+  http = inject(HttpClient);
+
   nextPage() {
     sessionStorage.setItem('formData', JSON.stringify(this.formData));
     this.router.navigate(['onboarding/duur'])
-    }
+    };
+
+  addPerson() {
+
+    // Save data to sessionstorage
+    sessionStorage.setItem('formData', JSON.stringify(this.formData));
+  
+  
+    // Decclare the API endpoint URL
+    const apiUrl = 'http://127.0.0.1:8000/api/family_member';
+  
+    // Send data to server using POST request.
+    this.http.post(apiUrl, this.formData).subscribe(
+      (response) => {
+        // log succesful response to console
+        console.log('Form succesfully submitted:', response);
+       
+        // reset form
+        this.formData = {
+          name: '',
+          last_name: '',
+          gender: '',
+          height: 0,
+          weight: 0,
+          diet: '',
+          date_of_birth: '' ,
+        }
+        
+      },
+      (error) => {
+        // handle error
+        console.error('Error submitting form:', error);
+        alert('Er is iets misgegaan met het formulier door te sturen. Probeer het alstublieft opnieuw.');        }
+    );
+  };
 }
+
