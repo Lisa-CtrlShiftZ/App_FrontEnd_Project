c63622adf3e38a219eb2f405047440cd9061c876
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2025-02-01 23:25:38 +0100

    fixed error in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { StorageComponent } from '.components/storage/storage.component'; // Adjust path if needed
+import { StorageComponent } from './components/storage/storage.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StorageComponent // Add your component here
+     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppComponent,
+    StorageComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
